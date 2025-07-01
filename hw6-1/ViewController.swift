cda0f0650e58f5ed20f71d2dfc87ea38bf502f63
ibrahimkatada <ibrahimkatada@icloud.com>
2025-07-01 19:57:24 +0300

    Initial Commit

---
+++ hw6-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw6-1
+//
+//  Created by Ибрагим Чахкиев on 01.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
