48745b2c4c1fc25a982b50d7ae23384a67eb4249
ibrahimkatada <ibrahimkatada@icloud.com>
2025-07-02 16:34:12 +0300

    1

---
+++ hw6-1/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  hw6-1
+//
+//  Created by Ибрагим Чахкиев on 02.07.2025.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    var userName: ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
